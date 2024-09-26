6846deb57d19b50d9ca92f7f5d188ca2a86a867f
Tsanta <priscillatsanta004@gmail.com>
2024-09-26 09:09:56 +0300

    ajout element dans les cases

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const PuzzleContainer = document.getElementById('puzzle')
-    const puzzleSize = 3
-    let cases = []
+    const PuzzleContainer = document.getElementById('puzzle');
+    const puzzleSize = 3;
+    let cases = [];
 
     function creationCasePuzzle() {
-        for (let i = 1 ; i <= puzzleSize * puzzleSize ; i++) {
-            
-            const casePuzzle = document.createElement("div");
-            casePuzzle.className = "casePuzzle"
 
-            if (i == 9) {
-                casePuzzle.classList.add("empty")
+        for (let i = 1; i < puzzleSize * puzzleSize; i++) {
+            cases.push(i);
+        }
+        cases.push(""); // Ajouter une case vide à la fin
+
+        cases.forEach((caseElement, index) => {
+            const casePuzzle = document.createElement("div");
+            casePuzzle.className = "casePuzzle";
+            casePuzzle.textContent = caseElement;
+
+            if (caseElement === "") {
+                casePuzzle.classList.add("empty"); // Ajout de la classe à l'élément DOM
             }
 
-            PuzzleContainer.appendChild(casePuzzle)
-        }
+            PuzzleContainer.appendChild(casePuzzle); // Ajouter l'élément DOM au container
+        });
+
     }
 
     creationCasePuzzle();
-})
+});
