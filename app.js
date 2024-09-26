0a59d8725091c49161da094e72bc46aae074c973
Harena <sarobidyharena21@gmail.com>
2024-09-26 08:03:27 +0300

    Creation element div pour les cases

--- app.js
+++ app.js
@@ -2,4 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const PuzzleContainer = document.getElementById('puzzle')
     const puzzleSize = 3
     let cases = []
+
+    function creationCasePuzzle() {
+        for (let i = 1 ; i <= puzzleSize * puzzleSize ; i++) {
+            
+            const casePuzzle = document.createElement("div");
+            casePuzzle.className = "casePuzzle"
+
+            if (i == 9) {
+                casePuzzle.classList.add("empty")
+            }
+
+            PuzzleContainer.appendChild(casePuzzle)
+        }
+    }
+
+    creationCasePuzzle();
 })
